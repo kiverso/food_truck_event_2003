8a97aded44550752aaa8584438c4f459de0517d1
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-15 10:57:34 -0600

    Add add_food_truck test

--- test/event_test.rb
+++ test/event_test.rb
@@ -13,6 +13,11 @@ class EventTest < Minitest::Test
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @event = Event.new("South Pearl Street Farmers Market")
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
   end
 
   def test_it_exists
@@ -24,42 +29,17 @@ class EventTest < Minitest::Test
     assert_equal expected, @event.name
     assert_equal [], @event.food_trucks
   end
+
+  def test_it_can_add_food_trucks
+    assert_equal [], @event.food_trucks
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expected = [@food_truck1, @food_truck2, @food_truck3]
+    assert_equal expected, @event.food_trucks
+  end
 end
 
-# pry(main)> event.food_trucks
-# #=> []
-#
-# pry(main)> food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-# #=> #<FoodTruck:0x00007fe1348a1160...>
-#
-# pry(main)> item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# pry(main)> item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-# pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-#
-# pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-#
-# pry(main)> food_truck1.stock(item1, 35)
-#
-# pry(main)> food_truck1.stock(item2, 7)
-#
-# pry(main)> food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
-# #=> #<FoodTruck:0x00007fe1349bed40...>
-#
-# pry(main)> food_truck2.stock(item4, 50)
-#
-# pry(main)> food_truck2.stock(item3, 25)
-#
-# pry(main)> food_truck3 = FoodTruck.new("Palisade Peach Shack")
-# #=> #<FoodTruck:0x00007fe134910650...>
-#
-# pry(main)> food_truck3.stock(item1, 65)
-#
 # pry(main)> event.add_food_truck(food_truck1)
 #
 # pry(main)> event.add_food_truck(food_truck2)
