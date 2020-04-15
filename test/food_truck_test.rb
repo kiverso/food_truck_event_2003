8711a49248afc36e8e6319f0c703344305f2137c
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-15 10:27:39 -0600

    Add stock test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -18,6 +18,13 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", @food_truck.name
     assert_equal ({}), @food_truck.inventory
   end
+
+  def test_it_can_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 30)
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+  end
 end
 
 
